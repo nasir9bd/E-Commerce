4eface7715cc09183a510d98bcdbdc94d5688464
nasir9bd <nasir9bd@gmail.com>
2014-10-20 09:57:20 +0600

    Added Catalogue

---
+++ classes/Catalogue.php
@@ -0,0 +1,9 @@
+<?php
+class Catalogue extends Application{
+    private $_table = 'categories';
+
+    public function getCategories(){
+        $sql = "SELECT * FROM `{$this->_table}` ORDER BY `name` ASC";
+        return $this->db->fetchAll($sql);
+    }
+}
--- template/_header.php
+++ template/_header.php
@@ -1,3 +1,11 @@
+<?php
+$objCatalogue = new Catalogue();
+$cats = $objCatalogue->getCategories();
+
+$objBusiness = new Business();
+$business = $objBusiness->getBusiness();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -6,13 +14,13 @@
     <meta name="description" content="Ecommerce website project" />
     <meta name="keywords" content="Ecommerce website project" />
     <meta http-equiv="imagetoolbar" content="no" />
-    <link href="/css/core.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/core.css"/>
 </head>
 <body>
 <div id="header">
     <div id="header_in">
 
-        <h5><a href="/">Business name</a></h5>
+        <h5><a href="/"><?php echo $business['name']; ?></a></h5>
     </div>
 </div>
 <div id="outer">
@@ -20,6 +28,17 @@
         <div id="left">
             <h2>Categories</h2>
             <ul id="navigation">
+                <?php
+                if(!empty($cats)){
+                    foreach($cats as $cat){
+                        echo "<li><a href=\"/?page=catalogue&amp;category=" .$cat['id']."\"";
+                        echo Helper::getActive(array('category'=>$cat['id']));
+                        echo ">";
+                        echo Helper::encodeHtml($cat['name']);
+                        echo "</a></li>";
+                    }
+                }
+                ?>
                 <li><a href="#">link</a></li>
             </ul>
         </div>
